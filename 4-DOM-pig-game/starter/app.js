4abdc3b861882c72e1f9ca2a1df2715b8fcd00a7
nathan <nathanjbro@gmail.com>
2020-09-03 17:44:27 +0300

    near end of module 4

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,13 +11,85 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  document.querySelector(`.dice`).style.display = "none";
 
-dice = Math.floor(Math.random()*6)+1
-console.log(dice)
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  gamePlaying = true;
+}
 
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
+init();
+
+//Message queue in JS engine: queue of events that are only processed AFTER execution stack is empty.
+// State definition: Condition of a system
+// Anonymous function - no name so it cannot be called again/outside of context where it is called.
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  if (gamePlaying) {
+    // 1. Random Number Generation
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display result
+    var diceDom = document.querySelector(`.dice`);
+    diceDom.style.display = "block";
+    diceDom.src = `dice-${dice}.png`; //setter - sets the value
+
+    // 3.  Update rounded score if rolled number !== 1
+    if (dice !== 1) {
+      // add score
+      roundScore += dice;
+      document.querySelector(
+        `#current-${activePlayer}`
+      ).textContent = roundScore; // setter
+    } else {
+      // Next Player
+      nextPlayer();
+    }
+  }
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  // 1. Add current score to global score
+  scores[activePlayer] += roundScore;
+  // 2. Update UI
+  document.querySelector(`#score-${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 3. Check if player won game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector(`#name-${activePlayer}`).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(`.player-${activePlayer}-panel`)
+      .classList.add("winner");
+    document
+      .querySelector(`.player-${activePlayer}-panel`)
+      .classList.remove("active");
+    gamePlaying = false;
+  } else {
+    // 4. Next Player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  // next player
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(`.player-0-panel`).classList.toggle("active");
+  document.querySelector(`.player-1-panel`).classList.toggle("active");
+  document.querySelector(".dice").style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
