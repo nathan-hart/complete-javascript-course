5a7f784a12029856af969eda803ae50b313d8737
nathan <nathanjbro@gmail.com>
2020-09-05 12:26:48 +0300

    finished module 4

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -12,13 +12,14 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer, gamePlaying, prevRoll;
-var winningScoreValue = document.querySelector('.winScore').value
+var winningScoreValue = document.querySelector(".winScore").value;
 
 function init() {
   scores = [0, 0];
   roundScore = 0;
   activePlayer = 0;
-  document.querySelector(`.dice`).style.display = "none";
+  document.querySelector(`.dice1`).style.display = "none";
+  document.querySelector(`.dice2`).style.display = "none";
 
   document.getElementById("score-0").textContent = "0";
   document.getElementById("score-1").textContent = "0";
@@ -43,14 +44,24 @@ init();
 document.querySelector(".btn-roll").addEventListener("click", function () {
   if (gamePlaying) {
     // 1. Random Number Generation
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
     // var dice = 6;
     // 2. Display result
-    var diceDom = document.querySelector(`.dice`);
-    diceDom.style.display = "block";
-    diceDom.src = `dice-${dice}.png`; //setter - sets the value
+    var diceDom1 = document.querySelector(`.dice1`);
+    var diceDom2 = document.querySelector(`.dice2`);
+    diceDom1.style.display = "block";
+    diceDom2.style.display = "block";
+    diceDom1.src = `dice-${dice1}.png`; //setter - sets the value
+    diceDom2.src = `dice-${dice2}.png`; //setter - sets the value
+    if (dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice2 + dice1;
+      document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
     // 3.  Update rounded score if rolled number !== 1
-    if (dice !== 1 && prevRoll + dice !== 12) {
+    /* //Challenge 2 if (dice1 !== 1 && prevRoll + dice !== 12) {
       // add score
       roundScore += dice;
       document.querySelector(
@@ -73,7 +84,7 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
       nextPlayer();
       prevRoll = 0;
 
-    }
+    } */
     // prevRoll = dice;
   }
 });
@@ -87,15 +98,16 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
       scores[activePlayer];
     // 3. Check if player won game
     var winningScore;
-    var input = document.querySelector('.winScore').value;
+    var input = document.querySelector(".winScore").value;
     if (input) {
-      winningScore = input
+      winningScore = input;
     } else {
       winningScore = 100;
     }
     if (scores[activePlayer] >= winningScore) {
       document.querySelector(`#name-${activePlayer}`).textContent = "Winner!";
-      document.querySelector(".dice").style.display = "none";
+      document.querySelector(".dice1").style.display = "none";
+      document.querySelector(".dice2").style.display = "none";
       document
         .querySelector(`.player-${activePlayer}-panel`)
         .classList.add("winner");
@@ -120,7 +132,8 @@ function nextPlayer() {
 
   document.querySelector(`.player-0-panel`).classList.toggle("active");
   document.querySelector(`.player-1-panel`).classList.toggle("active");
-  document.querySelector(".dice").style.display = "none";
+  document.querySelector(".dice1").style.display = "none";
+  document.querySelector(".dice2").style.display = "none";
 }
 
 document.querySelector(".btn-new").addEventListener("click", init);
